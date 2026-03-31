9cf9919fa681bf732755178ee137f4d626bfb194
Arnoud Plantinga <a.plantinga@etz.nl>
2026-03-31 11:55:39 +0200

    voeg polars toe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
+import polars as pl
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Hello, world!"
+st.write("Hello, world!")
+
+df = pl.DataFrame(
+    {
+        "a": [1, 2, 3],
+        "b": ["x", "y", "z"],
+    }
 )
+st.write(df)
